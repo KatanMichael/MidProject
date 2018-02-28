2b564c963e83bcc764dd40552c90b2efa0920649
katanmichael <michaelkatan@gmail.com>
2018-02-28 17:25:56 +0200

    Ui Finished-ish

---
+++ app/src/main/java/com/michaelkatan/midproject/AddNewProduct.java
@@ -0,0 +1,20 @@
+package com.michaelkatan.midproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by MichaelKatan on 28/02/2018.
+ */
+
+public class AddNewProduct extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addnew_layout);
+
+
+    }
+}
--- app/src/main/java/com/michaelkatan/midproject/MainActivity.java
+++ app/src/main/java/com/michaelkatan/midproject/MainActivity.java
@@ -1,15 +1,46 @@
 package com.michaelkatan.midproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
+    Button newProduct;
+    Button allProducts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        newProduct = findViewById(R.id.addProduct);
+        allProducts = findViewById(R.id.allProducts);
+
+
+        newProduct.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this,AddNewProduct.class);
+                startActivity(intent);
+            }
+        });
+
+        allProducts.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+
+            }
+        });
+
 
     }
 }
